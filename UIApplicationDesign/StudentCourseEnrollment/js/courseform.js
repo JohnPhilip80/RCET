52e89d6b3693a914062101c9a75eba8840212d22
JohnPhilip80 <rushtophilip@gmail.com>
2026-03-09 04:49:28 +0530

    Add files via upload

---
+++ UIApplicationDesign/StudentCourseEnrollment/js/courseform.js
@@ -0,0 +1,44 @@
+function saveCourse(){
+    if(document.getElementById("id").value){
+        updateCourse();
+    }
+    else{
+        createCourse();
+    }
+}
+function createCourse(){
+    let course = {
+        "title":document.getElementById("title").value,
+        "duration":document.getElementById("duration").value
+    };
+    httpRequest("http://localhost:3000/courses","POST",course).then(function(response){
+        window.location.href = "courselist.html";
+    });
+}
+function loadCourse(){
+    let requestParam = new URLSearchParams(window.location.search).get("id");
+    if(requestParam)
+    {
+        document.getElementById("courseFormTitle").textContent = "Edit Course";
+        httpRequest("http://localhost:3000/courses/" + requestParam ,"GET").then(function(course){
+            document.getElementById("id").value = course.id;
+            document.getElementById("title").value = course.title;
+            document.getElementById("duration").value = course.duration;
+        });
+    }
+    else{
+        document.getElementById("courseFormTitle").textContent = "Add Course"
+    }
+}
+function updateCourse(){
+    course = {
+        id:document.getElementById("id").value,
+        title:document.getElementById("title").value,
+        duration:document.getElementById("duration").value
+    };
+    httpRequest("http://localhost:3000/courses/" + document.getElementById("id").value,"PUT",course).then(function(response){
+        window.location.href = "courselist.html";
+    });
+}
+document.addEventListener('DOMContentLoaded', loadCourse);
+
